3bfd14ad9405f3238d73fcce8792c8df16b11ff9
Kaio Mourato <kaiomouratodm@hotmail.com>
2024-07-27 12:56:13 -0300

    ATIVIDADES E PROJETOS

--- PROJETOS/01.ListaDeCompras.js
+++ PROJETOS/01.ListaDeCompras.js
@@ -1,42 +1,100 @@
-/* O programa deve oferecer as
-seguintes funcionalidades:
->>>>>>>>>>>>• Adicionar um item: O usuário deve ser capaz de adicionar novos itens à
-lista de compras;
->>>>>>>>>>>>• Remover um item: O usuário deve ser capaz de remover itens da lista de
-compras;
-• Pesquisar item: O programa deve permitir que os usuários verifiquem se
-um determinado item já está na lista de compras.
-• Ordenar a lista: O programa deve permitir que o usuário ordene a lista de
-compras pelos nomes dos itens.
-• Exibir lista: O programa deve permitir a exibição de todos os itens da lista.
-• Limpar a lista: O programar deve permitir que o usuário remova todos os
-itens da lista de compras de uma vez.
-• Encerrar programa, utilize um laço de repetição para manter o programa
-ativo exibindo o menu de opções até o usuário escolher encerrar o programa*/
+let listaDeCompras = []
 
-let prompt = require ('prompt-sync')()
-
-let listaDeCompras = ["LEITE","PÃO","QUEIJO","OVOS","FRUTAS","VEGETAIS","CARNE","ARROZ","FEIJÃO","MACARRÃO"]
-
-function adicionarNovoItem(){
-    let itemNovo = prompt('Adicione o novo item: ')
-    listaDeCompras.push(itemNovo)
-}
-function retirarItem(){
-    console.log(listaDeCompras)
-    let indexDoRemovido = listaDeCompras.indexOf(prompt('Elemento a ser removido: '))
-    if (indexDoRemovido !== -1){
-        listaDeCompras.splice(indexDoRemovido,1)
+function maiusculo(a) {
+    if (typeof a === 'string') {
+        return a.toUpperCase()
+    } else if (Array.isArray(a)) {
+        return a.map(item => maiusculo(item))
     } else {
-    console.log('Elemento a ser removido não encontrado')}
-}
-function pesquisarItem (){
-    let indexPesquisa = listaDeCompras.indexOf(prompt('Pesquisar por: '))
-    if (indexPesquisa !== -1){
-        console.log('O elemento está na lista')
-    } else{
-        console.log('O elemento não está na lista')
-        }
+        return a
+    }
 }
 
-retirarItem()
+function adicionarNovoItem() {
+    console.log('* Evite o uso de acentos')
+    let itemNovo = prompt('Adicione o novo item: ')
+    listaDeCompras.push(maiusculo(itemNovo))
+    console.log(`Lista atualizada: ${listaDeCompras}`)
+}
+function retirarItem() {
+    console.log(`Analise qual elemento deve ser removido:
+                 ${listaDeCompras}`)
+    console.log('* Evite o uso de acentos')
+    let itemParaRemover = maiusculo(prompt('Remover: '))
+    let indexDoRemovido = listaDeCompras.indexOf(itemParaRemover)
+    if (indexDoRemovido !== -1) {
+        listaDeCompras.splice(indexDoRemovido, 1)
+        console.log(`Lista atualizada: ${listaDeCompras}`)
+    } else {
+        console.log('Elemento a ser removido não encontrado')
+    }
+}
+function pesquisarItem() {
+    console.log('* Evite o uso de acentos')
+    let itemPesquisado = prompt('Pesquisar por: ')
+    if (itemPesquisado) {
+        let itemPesquisadoMSA = maiusculo(itemPesquisado)
+        let listaDeComprasMSA = listaDeCompras.map(item => maiusculo(item))
+        if (listaDeComprasMSA.includes(itemPesquisadoMSA)) {
+            console.log('O item está na Lista')
+        } else {
+            console.log('O item não está na Lista')
+        }
+    }
+}
+function ordenarLista() {
+    listaDeCompras.sort((a, b) => maiusculo(a).localeCompare(maiusculo(b)))
+    listaDeCompras.forEach(item => {
+        console.log(item)
+    });
+}
+function exibirLista() {
+    listaDeCompras.forEach(item => {
+        console.log(item)
+    })
+}
+function limpaLista() {
+    listaDeCompras.length = 0
+    console.log(listaDeCompras)
+}
+
+while(true){
+    console.log(` 
+        ||| LISTA DE COMPRAS |||
+          MENU:
+              1 - Adicionar novo item à lista
+              2 - Retirar Item da Lista
+              3 - Pesquisar Item 
+              4 - Exibir Lista 
+              5 - Ordenar Lista
+              6 - ENCERRAR`)
+      let menu = parseInt(prompt(`
+          Ir: `))
+if (menu == 6){
+    console.log(`
+        ENCERRANDO...
+        `)
+    break
+}
+switch (menu){
+    case 1:
+        adicionarNovoItem()
+        break
+    case 2:
+        retirarItem()
+        break
+    case 3:
+        pesquisarItem()
+        break
+    case 4:
+        exibirLista()
+        break
+    case 5:
+        ordenarLista()
+        break
+    default:
+        console.log('Operação Inválida!')
+        break
+}
+}
+    
